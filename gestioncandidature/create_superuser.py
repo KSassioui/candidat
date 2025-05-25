e1cd42e37bcda1726c66074c931482125f7c537d
KSassioui <sas.khadija@gmail.com>
2025-05-25 03:51:52 +0100

    Force Railway to run migrations90

---
+++ gestioncandidature/create_superuser.py
@@ -0,0 +1,20 @@
+# gestioncandidature/create_superuser.py
+
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestioncandidature.settings")
+django.setup()
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="admin123"
+    )
+    print("✅ Superuser créé.")
+else:
+    print("ℹ Superuser déjà existant.")
